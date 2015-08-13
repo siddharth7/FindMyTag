5d3c2726c1b48cad1785394c293170a371d618c0
siddharthsingh <siddharth7.singh@gmail.com>
2015-08-13 13:54:19 +0530

    User page

---
+++ app/src/main/java/com/example/rohantiwari/tag/User_page.java
@@ -0,0 +1,14 @@
+package com.example.rohantiwari.tag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by siddharthsingh on 10/08/15.
+ */
+public class User_page extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_act);
+    }
+}
