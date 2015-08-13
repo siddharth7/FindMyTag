77d54f7713694bfb65a35c6f2c775497bd9d9395
siddharthsingh <siddharth7.singh@gmail.com>
2015-08-13 14:00:57 +0530

    User page added

--- app/src/main/java/com/example/rohantiwari/tag/Login.java
+++ app/src/main/java/com/example/rohantiwari/tag/Login.java
@@ -3,24 +3,40 @@ package com.example.rohantiwari.tag;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
 
-public class Login extends Activity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class Login extends Activity implements View.OnClickListener {
 
     private EditText mEmailLogin;
     private EditText mPasswordLogin;
     private TextView _signupLink;
     private Button mLoginbtn;
+    private String success_login="false";
+    public JSONObject json;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,105 +45,85 @@ public class Login extends Activity {
         mEmailLogin = (EditText) findViewById(R.id.email_login);
         mPasswordLogin = (EditText) findViewById(R.id.password_login);
         mLoginbtn = (Button) findViewById(R.id.btn_login);
-        _signupLink = (TextView)findViewById(R.id.link_login);
+        _signupLink = (TextView) findViewById(R.id.link_login);
+        mLoginbtn.setOnClickListener(this);
 
-        mLoginbtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
-
-        _signupLink.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // Start the Signup activity
-                Intent intent = new Intent(getApplicationContext(), SignUp.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    public void login() {
-
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        final ProgressDialog progressDialog = new ProgressDialog(Login.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-        String email = mEmailLogin.getText().toString();
-        String password = mPasswordLogin.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        //onLoginSuccess();
-                        onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
     }
     @Override
-    public void onBackPressed() {
-        // disable going back to the MainActivity
-        moveTaskToBack(true);
-    }
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                // call AsynTask to perform network operation on separate thread
+                ProgressDialog dialog = new ProgressDialog(Login.this);
+                dialog.setMessage("Authenticating..");
+                dialog.show();
+                new HttpAsyncTask().execute("http://192.168.56.74:8000/token/new.json");
+                dialog.dismiss();
+                Log.d("final result of login",success_login);
+                if(success_login=="true") {
+                    Intent i = new Intent(Login.this, User_page.class);
+                    startActivity(i);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Authentication error",Toast.LENGTH_SHORT).show();
+                }
 
-    public boolean validate() {
-        boolean valid = true;
+                break;
 
-        String email = mEmailLogin.getText().toString();
-        String password = mPasswordLogin.getText().toString();
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            mEmailLogin.setError("enter a valid email address");
-            valid = false;
-        } else {
-            mEmailLogin.setError(null);
         }
-
-        if (password.isEmpty() || password.length() < 4 ) {
-            mPasswordLogin.setError("greater than 4 alphanumeric characters");
-            valid = false;
-        } else {
-            mPasswordLogin.setError(null);
+    }
+    public String POST(String url) {
+        InputStream is = null;
+        String result = "";
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("username", mEmailLogin.getText().toString()));
+        nameValuePairs.add(new BasicNameValuePair("password", mPasswordLogin.getText().toString()));
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            if(is != null)
+                result = convertInputStreamToString(is);
+            else
+                result = "Did not work!";
+            json = new JSONObject(result);
+            //JSONObject json_LL = json.getJSONObject("LL");
+            success_login=json.getString("success");
+            Log.d("success value",success_login);
+            Log.d("HTTP", "HTTP: OK");
+            Log.d("token received",result);
+        } catch (Exception e) {
+            Log.e("HTTP", "Error in http connection " + e.toString());
         }
+        return result;
 
-        return valid;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return POST(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
